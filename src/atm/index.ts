b2d184bf5eba11405174f70dfdc649a89fb2e136
Muhammad Fuad <muhammad.fuad@smartfren.com>
2022-11-08 18:18:23 +0700

    Optimizing code

--- src/atm/index.ts
+++ src/atm/index.ts
@@ -35,72 +35,60 @@ const login = (commandObject: string) => {
 };
 
 const logout = () => {
-  if (currentUserData.name.length === 0) {
-    console.log(errorColor(`(!) You have not logged in yet`));
-  } else {
-    console.log(botColor(`Goodbye, ${currentUserData.name}!`));
-    currentUserData = {
-      name: "",
-      balance: 0,
-    };
-  }
+  console.log(botColor(`Goodbye, ${currentUserData.name}!`));
+  currentUserData = {
+    name: "",
+    balance: 0,
+  };
 };
 
 const deposit = (totalDeposit: string) => {
-  if (currentUserData.name.length !== 0) {
-    const modifyObject = totalDeposit.replace(/\$/g, "");
-    const toNumber = parseInt(modifyObject);
-    currentUserData.balance += toNumber;
-    console.log(botColor(`Your balance is $${currentUserData.balance}`));
-  } else {
-    console.log(errorColor(`(!) You have not logged in yet`));
-  }
+  const modifyObject = totalDeposit.replace(/\$/g, "");
+  const toNumber = parseInt(modifyObject);
+  currentUserData.balance += toNumber;
+  console.log(botColor(`Your balance is $${currentUserData.balance}`));
 };
 
 const transfer = (commandObject: string) => {
-  if (currentUserData.name.length !== 0) {
-    const recipientName = commandObject.split(" ")[0];
-    const totalTransfer = commandObject.replace(recipientName, "").trimStart();
-    const modifyObject = totalTransfer.replace(/\$/g, "");
-    const totalTransferToNumber = parseInt(modifyObject);
+  const recipientName = commandObject.split(" ")[0];
+  const totalTransfer = commandObject.replace(recipientName, "").trimStart();
+  const modifyObject = totalTransfer.replace(/\$/g, "");
+  const totalTransferToNumber = parseInt(modifyObject);
 
-    const foundData = ATMdata.find((d) => d.name === recipientName);
-    if (!foundData) {
-      console.log(errorColor(`(!) ${recipientName} not found!`));
-    } else {
-      if (currentUserData.balance > totalTransferToNumber) {
-        const newArr = ATMdata.map((customer) => {
-          // 👇️ add recipient balance
-          if (customer.name === recipientName) {
-            const currentBalance = customer.balance;
-            return {
-              ...customer,
-              balance: currentBalance + totalTransferToNumber,
-            };
-          }
-          // 👇️ reduce customer balance
-          if (customer.name === currentUserData.name) {
-            const currentBalance = customer.balance;
-            const newCustomerData = {
-              ...customer,
-              balance: currentBalance - totalTransferToNumber,
-            };
-            currentUserData = newCustomerData;
-            return newCustomerData;
-          }
-          return customer;
-        });
-        ATMdata = newArr;
-        console.log(
-          botColor(`Transferred $${totalTransferToNumber} to ${recipientName}`)
-        );
-        console.log(botColor(`Your balance $${currentUserData.balance}`));
-      } else {
-        console.log(errorColor(`(!) Your balance not enough to transfer`));
-      }
-    }
+  const foundData = ATMdata.find((d) => d.name === recipientName);
+  if (!foundData) {
+    console.log(errorColor(`(!) ${recipientName} not found!`));
   } else {
-    console.log(errorColor(`(!) You have not logged in yet`));
+    if (currentUserData.balance > totalTransferToNumber) {
+      const newArr = ATMdata.map((customer) => {
+        // 👇️ add recipient balance
+        if (customer.name === recipientName) {
+          const currentBalance = customer.balance;
+          return {
+            ...customer,
+            balance: currentBalance + totalTransferToNumber,
+          };
+        }
+        // 👇️ reduce customer balance
+        if (customer.name === currentUserData.name) {
+          const currentBalance = customer.balance;
+          const newCustomerData = {
+            ...customer,
+            balance: currentBalance - totalTransferToNumber,
+          };
+          currentUserData = newCustomerData;
+          return newCustomerData;
+        }
+        return customer;
+      });
+      ATMdata = newArr;
+      console.log(
+        botColor(`Transferred $${totalTransferToNumber} to ${recipientName}`)
+      );
+      console.log(botColor(`Your balance $${currentUserData.balance}`));
+    } else {
+      console.log(errorColor(`(!) Your balance not enough to transfer`));
+    }
   }
 };
 
@@ -116,30 +104,42 @@ const ATMProject = async () => {
     command = answered.split(" ")[0];
     obj = answered.replace(command, "").trimStart();
 
-    switch (command) {
-      case "login":
+    if (currentUserData.name.length !== 0) {
+      switch (command) {
+        case "login":
+          console.log(errorColor(`(!) You need to log out first`));
+          break;
+        case "deposit":
+          deposit(obj);
+          break;
+        case "logout":
+          logout();
+          break;
+        case "transfer":
+          transfer(obj);
+          break;
+        default:
+          console.log(errorColor(`(!) Command not found!`));
+          break;
+      }
+    } else {
+      if (command === "login") {
         login(obj);
-        ATMProject();
-        break;
-      case "deposit":
-        deposit(obj);
-        ATMProject();
-        break;
-      case "logout":
-        logout();
-        ATMProject();
-        break;
-      case "transfer":
-        transfer(obj);
-        ATMProject();
-        break;
-      default:
+      } else if (
+        command === "logout" ||
+        command === "deposit" ||
+        command === "transfer" ||
+        command === "withdraw"
+      ) {
+        console.log(errorColor(`(!) You have not logged in yet`));
+      } else {
         console.log(errorColor(`(!) Command not found!`));
-        ATMProject();
-        break;
+      }
     }
+    ATMProject();
   } catch (error) {
     console.log(error);
+    ATMProject();
   }
 };
 
